d45eff832c4de121043fc21c0abbe90f46c90ab7
beva3 <mizabienvenu@gmail.com>
2024-11-05 18:24:01 +0530

    allocation dinamique

--- persone.cpp
+++ persone.cpp
@@ -10,7 +10,7 @@ private:
 public:
     Person(string n, int a):name(n), age(a) {}
     ~Person() {
-        cout << "Destructor called for"<< name<< endl;
+        cout << "Destructor called for "<< name<< endl;
     }
     void display(){
         cout << "Name: " << name << ", Age: " << age << endl;
@@ -21,5 +21,10 @@ int main(){
     cout << "learn poo" <<endl;
     Person person1("John Doe", 30);
     person1.display();
+
+    Person *p_ptr = new Person("Raphael",45);
+    p_ptr->display();
+
+    delete p_ptr; // call destructor manually
     return 0;
 }
